2204a33d8fcebe741f839a4bfb2725f4960ae305
Taavi TLU <taavik@tlu.ee>
2022-03-21 20:33:48 +0200

    kodutoo

---
+++ 06_liides/Kodutoo/Enemy1.java
@@ -0,0 +1,9 @@
+package Kodutoo;
+
+public class Enemy1 extends EnemyClass implements Enemy {
+
+    public Enemy1() {
+        super(3, 3, "Vastane");
+    }
+
+}
---
+++ 06_liides/Kodutoo/Enemy2.java
@@ -0,0 +1,8 @@
+package Kodutoo;
+
+public class Enemy2 extends EnemyClass implements Enemy {
+    public Enemy2() {
+        super(10, 100, "Vastane2");
+    }
+
+}
